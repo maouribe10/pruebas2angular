2887e0686b3395b461aec561babe44606751e07e
oscarUribe10 <oscar.uribe@ceiba.com.co>
2019-11-06 16:55:11 -0500

    commit proyecto

---
+++ src/app/components/contact/contact.component.spec.ts
@@ -0,0 +1,62 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ContactComponent } from './contact.component';
+import { By } from '@angular/platform-browser';
+
+describe('ContactComponent', () => {
+  let component: ContactComponent;
+  let fixture: ComponentFixture<ContactComponent>;
+  let element: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContactComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ]
+    })
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(ContactComponent);
+      component = fixture.componentInstance;
+    });
+  }));
+
+  it(`should have as text 'contact page'`, async(() => {
+    expect(component.text).toEqual('contact page');
+  }));
+
+  it(`should set submitted to true`, async(() => {
+    component.onSubmit();
+    expect(component.submitted).toBeTruthy();
+  }))
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`boton no clickeado`, async(() => {
+    fixture.detectChanges();
+    spyOn(component, 'onSubmit');
+    let element: HTMLElement = fixture.debugElement.query(By.css('button')).nativeElement;
+    element.click();
+    expect(component.onSubmit).toHaveBeenCalledTimes(0);
+  }))
+
+
+
+  it(`boton cliqueado`, async(() => {
+    fixture.detectChanges();
+    component.contact.email = 'f@ceiba.com.co';
+    component.contact.name = 'holas';
+    component.contact.text = 'fdfff';
+    component.createForm();
+    fixture.detectChanges();
+    spyOn(component, 'onSubmit');
+    let element: HTMLElement = fixture.debugElement.query(By.css('button')).nativeElement;
+    element.click();
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
+
+  }));
+});
